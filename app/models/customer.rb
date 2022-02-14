cafc2d74e3bcc3f2d5699805d74a541b24ce8a80
Grasi Eduarda <grasieduarda775@gmail.com>
2022-02-14 14:44:36 -0300

    BUG

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ApplicationRecord
+  has many :facilities , dependent: :destroy
+  has many :operations , dependent: :destroy
+  has many :contacts , dependent: :destroy
+  has_and_belongs_to_many :operations, dependent: :destroy
 end
